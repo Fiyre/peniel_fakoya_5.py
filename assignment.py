29fff2e66903a0b790812877a9beb1ac70e6cd1a
Peniel <penielfakoya@hotmail.com>
2023-07-01 09:11:05 -0400

    final commit

---
+++ assignment.py
@@ -0,0 +1,7 @@
+people = ['Jane', 'John', 'Jack', 'Janet']
+sex = ['Female', 'Male', 'Male', 'Female']
+age = [23, 34, 16, 28]
+
+for person, gender, person_age in zip(people,sex,age):
+   interpolated_string = f"{person} the {gender} is {person_age} years old."
+   print(interpolated_string)
