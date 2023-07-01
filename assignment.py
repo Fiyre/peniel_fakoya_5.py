3c983aa469e715585b3b5393d85d84c3c97d7025
Peniel <penielfakoya@hotmail.com>
2023-07-01 11:21:37 -0400

    last commit

--- assignment.py
+++ assignment.py
@@ -1,3 +1,27 @@
+str = "The quick brown fox jumps over the lazy dog"
+print(str)
+print(len(str))
+print(str.upper())
+print(str.lower())
+print(str.title())
+
+str_rev = "The quick brown fox jumps over the lazy dog" [::-1]
+print(str_rev)
+print(str.title())
+print(str.count("a"))
+print(str.count("the"))
+print(str.replace("the", "a"))
+
+freq = {}
+for i in str:
+        if i in freq:
+               freq[i] += 1
+        else:
+             freq[i] = 1
+	 
+print(freq)
+
+
 people = ['Jane', 'John', 'Jack', 'Janet']
 sex = ['Female', 'Male', 'Male', 'Female']
 age = [23, 34, 16, 28]
